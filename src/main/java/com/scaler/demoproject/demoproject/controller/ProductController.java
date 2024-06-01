6f8a016426e3a2256df00c3a1c6ee307fbf03ab6
murali <saimurali234@gmail.com>
2024-06-01 14:04:20 +0530

    connecting to the mysql database and creating tables

--- src/main/java/com/scaler/demoproject/demoproject/controller/ProductController.java
+++ src/main/java/com/scaler/demoproject/demoproject/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.scaler.demoproject.demoproject.controller;
 import com.scaler.demoproject.demoproject.dto.FakeStoreProductDto;
 import com.scaler.demoproject.demoproject.model.Product;
 import com.scaler.demoproject.demoproject.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +24,8 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public FakeStoreProductDto getFakeStoreProductDto() {
-        return productService.getDto();
+    public ResponseEntity<FakeStoreProductDto> getFakeStoreProductDto() {
+        return new ResponseEntity<>(productService.getDto(), HttpStatus.NOT_FOUND);
     }
 
 //    @GetMapping("/products/{id}")
--- src/main/java/com/scaler/demoproject/demoproject/model/Category.java
+++ src/main/java/com/scaler/demoproject/demoproject/model/Category.java
@@ -1,23 +1,21 @@
 package com.scaler.demoproject.demoproject.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Product extends BaseModel {
+public class Category extends BaseModel {
     private String title;
-    private String description;
-    private double price;
-    private String imageUrl;
-    @ManyToOne
-    private Category category;
-
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
